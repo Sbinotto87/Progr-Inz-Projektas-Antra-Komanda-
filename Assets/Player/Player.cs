6568ec91406628f642e55c731a0e85fa23fc46ab
EmilijaCe <c.emilija5@gmail.com>
2026-05-27 17:56:07 +0300

    Improved tool overlay

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -102,6 +102,14 @@ public class Player : MonoBehaviour
     public int sprintLockCount = 0;
     public int jumpLockCount = 0;
     
+    [Header("Meshes & Materials")]
+    public Mesh pickaxeMesh;
+    public Mesh axeMesh;
+    public Mesh shovelMesh;
+    public Mesh swordMesh;
+    public Material iron_material;
+    public Material stone_material;
+    
     private bool methodCalled = false;
 
     void Start()
